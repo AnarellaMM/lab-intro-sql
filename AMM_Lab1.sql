377ee24a3a1f37ad345c70382e97321e175b4747
AnarellaMM <131594247+AnarellaMM@users.noreply.github.com>
2023-05-20 13:38:01 +0200

    Add files via upload

---
+++ AMM_Lab1.sql
@@ -0,0 +1,25 @@
+use sakila;
+
+select * from actor;
+select * from film;
+select * from customer;
+
+select title
+from sakila.film; 
+
+select name as laguage
+from language;
+
+select store_id
+from store;
+
+select staff_id
+from staff;
+
+select first_name
+from staff;
+
+
+
+
+
